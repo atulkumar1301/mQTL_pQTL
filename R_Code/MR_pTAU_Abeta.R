63ab4e353708872f6b1bd87058cd9a72df28dd93
ATUL KUMAR <atul.0298@gmail.com>
2025-08-13 14:25:29 +0200

    Update MR_pTAU_Abeta.R

--- R_Code/MR_pTAU_Abeta.R
+++ R_Code/MR_pTAU_Abeta.R
@@ -4,7 +4,7 @@ library(data.table)
 ### read in exposure data (pQTLs). Default settings and column names shown below, not all of this information is essential in MR, but will need at least SNP, beta, 
 #se, effect allele, (eaf ideally for harmonisation).
 
-df <- fread ("/Volumes/Macintosh HD/Users/akumar/OneDrive - University of Eastern Finland/Work/Projects/mQTL_pQTL/Metabolomics/MR_Study/mQTL_Results_No_Pleiotropy_e-11.txt")
+df <- fread ("~/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Work/Projects/mQTL_pQTL/Metabolomics/MR_Study/mQTL_Results_No_Pleiotropy_e-11.txt")
 
 df_1 <- data.frame(df)
 
@@ -23,6 +23,8 @@ Exposure_List <- unique (Exp_dat_Full$exposure)
 
 edf_All <- data.frame()
 edf_single <- data.frame()
+het_stat <- data.frame()
+hor_plei_stat <- data.frame()
 
 for (metabolites in Exposure_List) {
   sub <- subset (Exp_dat_Full, exposure == metabolites)
@@ -31,7 +33,7 @@ for (metabolites in Exposure_List) {
   ### read in and match outcome data to exposure data 
   
   outcome_dat_AD=read_outcome_data (
-    filename = "/Volumes/Macintosh HD/Users/akumar/OneDrive - University of Eastern Finland/Tools/GWAS_Summary_Statistics/Summary_Data_AD_A_beta.txt",
+    filename = "~/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Tools/GWAS_Summary_Statistics/Summary_Data_AD_p_tau.txt",
     snps = sub$SNP,
     sep = "\t",
     snp_col = "SNP",
@@ -57,8 +59,21 @@ for (metabolites in Exposure_List) {
   ## single SNP MR - Wald ratio per IV to look at 
   res_single <- mr_singlesnp(dat)
   edf_single <- rbind (edf_single, res_single)
+  
+  #Sensitivity analyses
+  #Heterogeneity statistics
+  het <- mr_heterogeneity(dat)
+  het_stat <- rbind (het_stat, het)
+  
+  #Horizontal pleiotropy
+  hor_plei <- mr_pleiotropy_test(dat)
+  hor_plei_stat <- rbind (hor_plei_stat, hor_plei)
 }
 
-write.table (edf_All, file = paste0 ("/Volumes/Macintosh HD/Users/akumar/OneDrive - University of Eastern Finland/Work/Projects/mQTL_pQTL/Metabolomics/MR_Study/MR_Results_Abeta_All.txt"), sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
+write.table (edf_All, file = paste0 ("~/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Work/Projects/mQTL_pQTL/Metabolomics/MR_Study/MR_Results_Abeta_All.txt"), sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
 
-write.table (edf_single, file = paste0 ("/Volumes/Macintosh HD/Users/akumar/OneDrive - University of Eastern Finland/Work/Projects/mQTL_pQTL/Metabolomics/MR_Study/MR_Results_Abeta_Single.txt"), sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
+write.table (edf_single, file = paste0 ("~/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Work/Projects/mQTL_pQTL/Metabolomics/MR_Study/MR_Results_Abeta_Single.txt"), sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
+
+write.table (het_stat, file = paste0 ("~/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Work/Projects/mQTL_pQTL/Metabolomics/MR_Study/MR_Results_Heterogeneity_pTau.txt"), sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
+
+write.table (hor_plei_stat, file = paste0 ("~/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Work/Projects/mQTL_pQTL/Metabolomics/MR_Study/MR_Results_pleiotropy_pTau.txt"), sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
